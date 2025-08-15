513046c0407227b4b6f1cfbbefdd33aa0f827620
ADITHYA787 <adithyanambiar77@gmail.com>
2025-08-15 09:16:55 -0400

    Add initial project files

--- customer_churn.py
+++ customer_churn.py
@@ -1,3 +1,5 @@
+#import libraies 
+
 import numpy as np  # Imports the NumPy library for numerical operations and array handling.
 import pandas as pd  # Imports the pandas library for data manipulation and analysis.
 import matplotlib.pyplot as plt  # Imports Matplotlib's pyplot for creating static visualizations.
@@ -28,3 +30,193 @@ df.describe().info()
 df.describe()
 df.info()
 df.isnull().sum()
+
+
+
+
+# Model Build 
+# Split model into training and test set
+#Split Dataset into Training set and Test set
+
+X = df.drop(columns=["churn"])
+y = df["churn"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+#Handling the Imbalanced dataset
+from imblearn.over_sampling import SMOTE
+
+print('Before upsampling count of label 0 {}'.format(sum(y_train==0)))
+print('Before upsampling count of label 1 {}'.format(sum(y_train==1)))
+
+# Minority Over Sampling Technique
+sm = SMOTE(sampling_strategy = 1, random_state=1)   
+X_train_s, y_train_s = sm.fit_resample(X_train, y_train.ravel())
+                                         
+print('After upsampling count of label 0 {}'.format(sum(y_train_s==0)))
+print('After upsampling count of label 1 {}'.format(sum(y_train_s==1)))
+
+#Model Evaluation with Cross Validation
+# Libraries
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn import feature_selection
+from sklearn import model_selection
+from sklearn import metrics
+
+from sklearn.linear_model import LogisticRegressionCV, RidgeClassifierCV, SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, GradientBoostingClassifier, RandomForestClassifier 
+from xgboost import XGBClassifier
+
+
+#Initialize the models
+models=[
+    #Ensemble
+    AdaBoostClassifier(),
+    BaggingClassifier(),
+    GradientBoostingClassifier(),
+    RandomForestClassifier(),
+    
+    #Linear Models
+    LogisticRegressionCV(),
+    RidgeClassifierCV(),
+    
+    #Nearest Neighbour
+    KNeighborsClassifier(),
+    
+    #XGBoost
+    XGBClassifier()
+]
+
+metrics_cols = ['model_name','test_accuracy','test_precision','test_recall','test_f1']
+
+model_name=[]
+test_acuracy=[]
+test_precision=[]
+test_recall=[]
+test_f1=[]
+
+
+# Model Evaluation
+
+scoring = ['accuracy','precision', 'recall', 'f1']
+
+for model in models:
+    cv_results = model_selection.cross_validate(model, X, y, cv=5, 
+                                                scoring=scoring, return_train_score=True)
+    model_name.append(model.__class__.__name__)
+    test_acuracy.append(round(cv_results['test_accuracy'].mean(),3)*100)
+    test_precision.append(round(cv_results['test_precision'].mean(),3)*100)
+    test_recall.append(round(cv_results['test_recall'].mean(),3)*100)
+    test_f1.append(round(cv_results['test_f1'].mean(),3)*100)
+
+metrics_data = [model_name, test_acuracy, test_precision, test_recall, test_f1]
+m = {n:m for n,m in zip(metrics_cols,metrics_data)}
+model_metrics = pd.DataFrame(m)
+model_metrics = model_metrics.sort_values('test_accuracy', ascending=False)
+metrics_styled = model_metrics.style.background_gradient(subset=['test_accuracy', 'test_f1'], cmap='summer')
+metrics_styled
+
+#RandomForestClassifier
+final_model=RandomForestClassifier()
+final_model.fit(X_train, y_train)
+train_pred = final_model.predict(X_train)
+test_pred = final_model.predict(X_test)
+
+final_model.score(X_test, y_test)
+
+#XGBClassifier
+final_model=XGBClassifier()
+final_model.fit(X_train, y_train)
+train_pred = final_model.predict(X_train)
+test_pred = final_model.predict(X_test)
+
+final_model.score(X_test, y_test)
+
+#Feature Importance of XGBoost Model
+
+#Importance_type
+#‘weight’ - the number of times a feature is used to split the data across all trees.
+#‘gain’ - the average gain across all splits the feature is used in.
+
+print("Feature Importance Gain")
+feature_important = final_model.get_booster().get_score(importance_type="gain")
+keys = list(feature_important.keys())
+values = list(feature_important.values())
+
+data = pd.DataFrame(data=values, index=keys, columns=["score"]).sort_values(by = "score", ascending=False)
+data.nlargest(20, columns="score").plot(kind='barh', figsize = (20,10)) ## plot top 40 features
+
+print("Feature Importance Weight")
+feature_important = final_model.get_booster().get_score(importance_type='weight')
+keys = list(feature_important.keys())
+values = list(feature_important.values())
+
+data = pd.DataFrame(data=values, index=keys, columns=["score"]).sort_values(by = "score", ascending=False)
+data.nlargest(20, columns="score").plot(kind='barh', figsize = (20,10)) ## plot top 40 features
+
+
+#Create Simpler models with less but more important features
+#select the columns
+cols_to_drop = ['preferredlogindevice_Computer', 'preferredlogindevice_Mobile Phone', 'preferredlogindevice_Phone', 
+                'preferredpaymentmode_CC', 'preferredpaymentmode_COD', 'preferredpaymentmode_Cash on Delivery', 'preferredpaymentmode_Credit Card', 
+                'preferredpaymentmode_Debit Card', 'preferredpaymentmode_E wallet', 'preferredpaymentmode_UPI', 'preferedordercat_Fashion', 
+                'preferedordercat_Grocery', 'preferedordercat_Laptop & Accessory', 'preferedordercat_Mobile', 'preferedordercat_Mobile Phone', 'preferedordercat_Others' ]
+X.drop(cols_to_drop, axis = 1, inplace = True)
+
+X_train.drop(cols_to_drop, axis = 1, inplace = True)
+X_test.drop(cols_to_drop, axis = 1, inplace = True)
+
+X.head()
+
+#Evaluate the Best Model
+# Convert the Pandas datafram to NumPy array for the XGBoost classifier. 
+# The conversion helps in loading and predicting values in the flask app.
+
+X_test = X_test.values
+X_train = X_train.values
+
+# Run the model
+final_model=XGBClassifier()
+final_model.fit(X_train, y_train)
+train_pred = final_model.predict(X_train)
+test_pred = final_model.predict(X_test)
+
+final_model.score(X_test, y_test)
+
+# Confusion Matrix
+cm = confusion_matrix(y_test, test_pred, labels=final_model.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=final_model.classes_)
+disp.plot()
+plt.title('Confusion Matrix of Random Forest Classifier')
+plt.show()
+
+#model predictions 
+# Make predictions on the test set
+y_pred = final_model.predict(X_test)
+
+# Create a list to store the actual and predicted values
+results = []
+
+# Store the actual and predicted log_price values in the list
+for actual, predicted in zip(y_test, y_pred):
+    results.append((actual, predicted))
+
+# Convert the list into a DataFrame
+XGBoost_df = pd.DataFrame(results, columns=['Actual', 'Predicted'])
+
+# Print the DataFrame
+XGBoost_df.tail(10)
+
+#Save the model
+import pickle
+pickle.dump(final_model,open('end_to_end_deployment/models/churn_prediction_model.pkl','wb'))
+#save the data columns
+import json
+
+columns = {'data_columns' : [col.lower() for col in X.columns]}
+
+with open("end_to_end_deployment/models/columns.json","w") as f:
+    f.write(json.dumps(columns))    
